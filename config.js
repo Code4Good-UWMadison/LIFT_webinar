87e0471508fdabc7e968d3e11e7edcd30942cc0c
Chentao Wang <cwang556@wisc.edu>
2020-08-09 22:41:25 -0500

    update

--- config.js
+++ config.js
@@ -22,7 +22,25 @@ const config = {
   sidebar: {
     forcedNavOrder: [
       '/introduction', // add trailing slash if enabled above
-      '/1',
+      '/01',
+      '/02',
+      '/03',
+      '/04',
+      '/05',
+      '/06',
+      '/07',
+      '/08',
+      '/09',
+      '/10',
+      '/11',
+      '/12',
+      '/13',
+      '/13.5',
+      '/14',
+      '/15',
+      '/16',
+      '/17',
+      '/18',
     ],
     // collapsedNav: [
     //   '/codeblock', // add trailing slash if enabled above
@@ -37,7 +55,7 @@ const config = {
     title: 'Gatsby Gitbook Boilerplate | Hasura',
     description: 'Documentation built with mdx. Powering hasura.io/learn ',
     ogImage: null,
-    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    docsLocation: 'https://github.com/Code4Good-UWMadison/LIFT_webinar/tree/master/content',
     favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
   },
   pwa: {
---
+++ content/components/4-game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game1 extends Component {
+  render() {
+    return (
+      <div>
+        <div style={{ display: 'flex', maxWidth: 900 }}></div>
+      </div>
+    );
+  }
+}
+
+export default Game1;
