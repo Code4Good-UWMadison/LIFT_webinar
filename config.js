ae3382019ab220c6d45f4e71d9870b07a269ebd6
Chentao Wang <cwang556@wisc.edu>
2020-09-29 15:10:02 -0500

    fix bug

--- config.js
+++ config.js
@@ -1,82 +1,78 @@
 const config = {
-  gatsby: {
-    pathPrefix: '/',
-    siteUrl: 'https://hasura.io',
-    gaTrackingId: null,
-    trailingSlash: false,
-  },
-  header: {
-    // logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    // logoLink: 'https://hasura.io/learn/',
-    title: 'Project',
-    githubUrl: 'https://github.com/Code4Good-UWMadison/LIFT_webinar',
-    // links: [{ text: '', link: '' }],
-    search: {
-      enabled: false,
-      indexName: '',
-      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
-      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    gatsby: {
+        pathPrefix: '/',
+        siteUrl: 'https://hasura.io',
+        gaTrackingId: null,
+        trailingSlash: false,
     },
-  },
-  sidebar: {
-    forcedNavOrder: [
-      '/introduction', // add trailing slash if enabled above
-      '/01',
-      '/02',
-      '/03',
-      '/04',
-      '/05',
-      '/06',
-      '/07',
-      '/08',
-      '/09',
-      '/10',
-      '/11',
-      '/12',
-      '/13',
-      '/13.5',
-      '/14',
-      '/15',
-      '/16',
-      '/17',
-      '/18',
-    ],
-    // collapsedNav: [
-    //   '/codeblock', // add trailing slash if enabled above
-    // ],
-    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
-    frontline: false,
-    ignoreIndex: true,
-    title:
-      "<a href='https://www.coding4good.net'>Coding for Good</a>&nbspX&nbsp<a href='https://www.liftdane.org'>LIFT Dane</a>",
-  },
-  siteMetadata: {
-    title: 'Gatsby Gitbook Boilerplate | Hasura',
-    description: 'Documentation built with mdx. Powering hasura.io/learn ',
-    ogImage: null,
-    docsLocation: 'https://github.com/Code4Good-UWMadison/LIFT_webinar/tree/master/content',
-    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
-  },
-  pwa: {
-    enabled: false, // disabling this will also remove the existing service worker.
-    manifest: {
-      name: 'Gatsby Gitbook Starter',
-      short_name: 'GitbookStarter',
-      start_url: '/',
-      background_color: '#6b37bf',
-      theme_color: '#6b37bf',
-      display: 'standalone',
-      crossOrigin: 'use-credentials',
-      icons: [
-        {
-          src: 'src/pwa-512.png',
-          sizes: `512x512`,
-          type: `image/png`,
+    header: {
+        // logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
+        // logoLink: 'https://hasura.io/learn/',
+        title: 'Project',
+        githubUrl: 'https://github.com/Code4Good-UWMadison/LIFT_webinar',
+        // links: [{ text: '', link: '' }],
+        search: {
+            enabled: false,
+            indexName: '',
+            algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+            algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+            algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
         },
-      ],
     },
-  },
+    sidebar: {
+        forcedNavOrder: [
+            '/introduction', // add trailing slash if enabled above
+            '/01',
+            '/02',
+            '/03',
+            '/04',
+            '/05',
+            '/06',
+            '/07',
+            '/08',
+            '/09',
+            '/10',
+            '/11',
+            '/12',
+            '/13',
+            '/14',
+            '/15',
+            '/16',
+            '/17',
+            '/18',
+        ],
+        // collapsedNav: [
+        //   '/codeblock', // add trailing slash if enabled above
+        // ],
+        links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+        frontline: false,
+        ignoreIndex: true,
+        title: "<a href='https://www.coding4good.net'>Coding for Good</a>&nbspX&nbsp<a href='https://www.liftdane.org'>LIFT Dane</a>",
+    },
+    siteMetadata: {
+        title: 'Gatsby Gitbook Boilerplate | Hasura',
+        description: 'Documentation built with mdx. Powering hasura.io/learn ',
+        ogImage: null,
+        docsLocation: 'https://github.com/Code4Good-UWMadison/LIFT_webinar/tree/master/content',
+        favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+    },
+    pwa: {
+        enabled: false, // disabling this will also remove the existing service worker.
+        manifest: {
+            name: 'Gatsby Gitbook Starter',
+            short_name: 'GitbookStarter',
+            start_url: '/',
+            background_color: '#6b37bf',
+            theme_color: '#6b37bf',
+            display: 'standalone',
+            crossOrigin: 'use-credentials',
+            icons: [{
+                src: 'src/pwa-512.png',
+                sizes: `512x512`,
+                type: `image/png`,
+            }, ],
+        },
+    },
 };
 
 module.exports = config;
