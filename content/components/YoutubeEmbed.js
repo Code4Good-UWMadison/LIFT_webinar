8bc3879a6242642a8b57b35141104732ac371f36
Kartikeye Khanna <65245684+kkartikeye@users.noreply.github.com>
2021-08-25 12:03:54 +0530

    Added Youtube Video Embedding backend

---
+++ content/components/YoutubeEmbed.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+function YoutubeApp() {
+  return (
+    <div>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=b94MPIwNd9s"
+      />
+    </div>
+  )
+}
+
+export default YoutubeApp
